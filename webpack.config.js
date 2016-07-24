f22aee80a2b0223b568614848ceabf665e6acbc6
Muzietto <marco_faustinelli@yahoo.com>
2016-07-24 14:28:33 +0100

    adding babel and a minimal webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  entry: './src/main.js',
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'backend.js'
+  },
+  externals: nodeModules,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0'/*, 'react'*/]
+        }
+      },
+      { test: /\.json$/, loader: "json" }
+    ]
+  }
+}
